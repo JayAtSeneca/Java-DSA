bef4d67b571257979a277be25ba13018ce26f182
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-09-02 13:57:53 -0400

    sum in java

---
+++ VariablesAndDataTypes/dataType.java
@@ -0,0 +1,9 @@
+package VariablesAndDataTypes;
+
+public class dataType {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        System.out.println(a+b);
+    }
+}
