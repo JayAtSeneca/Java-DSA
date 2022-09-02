5f0282fd93ff3abd468b7ac6b93eed9d24a63d0a
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-09-02 14:07:42 -0400

    Input in java for whole line with nextLine

---
+++ VariablesAndDataTypes/Input.java
@@ -0,0 +1,11 @@
+package VariablesAndDataTypes;
+import java.util.*;
+
+public class Input {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println(input);
+        sc.close();
+    }
+}
