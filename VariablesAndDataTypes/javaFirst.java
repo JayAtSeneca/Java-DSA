5a392808ec14d59be34176455ec7b53e2741e89f
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-09-02 12:59:12 -0400

    pattern in java

--- VariablesAndDataTypes/javaFirst.java
+++ VariablesAndDataTypes/javaFirst.java
@@ -2,8 +2,9 @@ package VariablesAndDataTypes;
 
 public class javaFirst {
     public static void main(String[] args){
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
     }
 }
