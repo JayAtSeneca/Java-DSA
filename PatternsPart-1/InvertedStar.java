aef7b3a11f2bc94a2d72a8e8c38f1b79e0d779ec
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-09-06 19:49:37 -0400

    Inverted star pattern

--- PatternsPart-1/InvertedStar.java
+++ PatternsPart-1/InvertedStar.java
@@ -1,12 +1,12 @@
 import java.util.*;
 
-public class StarPattern {
-    public static void main(String[] args){
+public class InvertedStar {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Positive Integer: ");
         int input = sc.nextInt();
-        for(int i = 1; i<=input;i++){
-            for(int j = 1; j<=i;j++){
+        for (int i = input; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
