87893c2760e82e15297e47cb0accdde0d03c1236
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-09-12 18:17:34 -0400

    Inverted pyramid with numbers pattern

---
+++ patternsPart-2/InvertedNumbers.java
@@ -0,0 +1,14 @@
+public class InvertedNumbers {
+    public static void inverted(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        inverted(5);
+    }
+}
