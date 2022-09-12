e681180e69dcd569754732da14af8a772f0efaf0
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-09-12 12:39:31 -0400

    average of three numbers

---
+++ functionsAndMethods/Avg.java
@@ -0,0 +1,9 @@
+public class Avg {
+    public static double avg(int a, int b, int c){
+        return (a+b+c)/3;
+    }
+    public static void main(String[] args){
+        double average = avg(3,5,7);
+        System.out.print(average + "is the average");
+    }
+}
