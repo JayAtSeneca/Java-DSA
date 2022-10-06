e14494dc476b089c67307eaf2992a89dfcbd5979
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-10-06 15:23:05 -0400

    largest in the array in java

--- Arrays/Largest.java
+++ Arrays/Largest.java
@@ -1,4 +1,5 @@
-public class Arr{
+// import java.util.*;
+public class Largest {
     public static int getLargest(int numbers[]){
         int large = Integer.MIN_VALUE;
         for(int i = 0; i<numbers.length;i++){
@@ -8,7 +9,7 @@ public class Arr{
         }
         return large;
     }
-    public static void main(String[] args){
+    public static void main(String args[]){
         int numbers[] = {1,2,6,3,5};
         System.out.println(getLargest(numbers));
     }
