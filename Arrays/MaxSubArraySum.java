f64e8c7dad7332230bb2b5e11fd43ff07d44e739
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-10-07 22:58:17 -0400

    max sub array sum with prefix array

--- Arrays/MaxSubArraySum.java
+++ Arrays/MaxSubArraySum.java
@@ -1,24 +1,46 @@
 public class MaxSubArraySum {
-    public static int subArraySum(int numbers[]){
+    // public static int subArraySum(int numbers[]){
+    // int currSum = 0;
+    // int maxSum = 0;
+    // for(int i=0;i<numbers.length;i++){
+    // for(int j = i; j<numbers.length; j++){
+    // currSum = 0;
+    // for(int k=i;k<=j;k++){
+    // currSum += numbers[k];
+    // }
+    // if(maxSum < currSum){
+    // maxSum = currSum;
+    // }
+
+    // }
+
+    // }
+    // return maxSum;
+    // }
+    public static int subArraySum(int numbers[]) {
         int currSum = 0;
         int maxSum = 0;
-        for(int i=0;i<numbers.length;i++){
-            for(int j = i; j<numbers.length; j++){
-                currSum = 0;
-                for(int k=i;k<=j;k++){
-                    currSum += numbers[k];
-                }
-                if(maxSum < currSum){
+        int prefix[] = new int[numbers.length];
+        prefix[0] = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            prefix[i] = prefix[i - 1] + numbers[i];
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i; j < numbers.length; j++) {
+                currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i - 1];
+
+                if (maxSum < currSum) {
                     maxSum = currSum;
                 }
-                
+
             }
-        
+
         }
         return maxSum;
     }
+
     public static void main(String[] args) {
-        int numbers[] = {2,4,6,8,10};
+        int numbers[] = { 2, 4, 6, 8, 10 };
         System.out.print(subArraySum(numbers));
     }
 }
