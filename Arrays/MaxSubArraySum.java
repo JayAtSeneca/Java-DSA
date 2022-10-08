677307ad65ec297eda4bad80a927e8cf3322e6c0
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-10-08 13:06:50 -0400

    max subarray sum kadane's algorithm

--- Arrays/MaxSubArraySum.java
+++ Arrays/MaxSubArraySum.java
@@ -17,30 +17,42 @@ public class MaxSubArraySum {
     // }
     // return maxSum;
     // }
-    public static int subArraySum(int numbers[]) {
+    // public static int subArraySum(int numbers[]) {
+    //     int currSum = 0;
+    //     int maxSum = 0;
+    //     int prefix[] = new int[numbers.length];
+    //     prefix[0] = numbers[0];
+    //     for (int i = 1; i < numbers.length; i++) {
+    //         prefix[i] = prefix[i - 1] + numbers[i];
+    //     }
+    //     for (int i = 0; i < numbers.length; i++) {
+    //         for (int j = i; j < numbers.length; j++) {
+    //             currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i - 1];
+
+    //             if (maxSum < currSum) {
+    //                 maxSum = currSum;
+    //             }
+
+    //         }
+
+    //     }
+    //     return maxSum;
+    // }
+    public static int kadanes(int numbers[]){
+        int maxSum = Integer.MIN_VALUE;
         int currSum = 0;
-        int maxSum = 0;
-        int prefix[] = new int[numbers.length];
-        prefix[0] = numbers[0];
-        for (int i = 1; i < numbers.length; i++) {
-            prefix[i] = prefix[i - 1] + numbers[i];
-        }
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i; j < numbers.length; j++) {
-                currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i - 1];
-
-                if (maxSum < currSum) {
-                    maxSum = currSum;
-                }
-
+        for(int i =0; i<numbers.length;i++){
+            currSum = currSum + numbers[i];
+            if(currSum<0){
+                currSum = 0;
             }
-
+            maxSum = Math.max(currSum, maxSum);
         }
         return maxSum;
     }
 
     public static void main(String[] args) {
-        int numbers[] = { 2, 4, 6, 8, 10 };
-        System.out.print(subArraySum(numbers));
+        int numbers[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+        System.out.print(kadanes(numbers));
     }
 }
