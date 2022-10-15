6a821b9f9a8e9d44e2efbf1e4efc21b128abf019
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-10-14 23:58:17 -0400

    odd or even function

--- Bitwise Operators/Bitwise.java
+++ Bitwise Operators/Bitwise.java
@@ -1,6 +1,13 @@
 public class Bitwise{
+    public static void evenOrOdd(int n){
+        if((n&1)==0){
+            System.out.println("Even Number");
+        }
+        else{
+            System.out.println("Odd Number");
+        }
+    }
     public static void main(String[] args) {
-        // Bitwise AND: help in find Odd or even number
-        System.out.println((50&1));
+        evenOrOdd(101);
     }
 }
