4d4e141614910de8b4b8d58b0a43d60a38ba99de
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-10-24 16:46:09 -0400

    writing a class in java

---
+++ OOP/Pens.java
@@ -0,0 +1,13 @@
+public class Pens{
+
+}
+class Pen{
+    String color;
+    int tip;
+    void setColor(String newColor){
+        this.color = newColor;
+    }
+    void setTip(int newTip){
+        this.tip = newTip;
+    }
+}
