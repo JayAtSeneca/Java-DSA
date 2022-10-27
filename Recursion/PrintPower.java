9f828405ad71557105c09d2955f6fc6f56200d5c
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-10-27 15:41:45 -0400

    print x to power n with log n run time

--- Recursion/PrintPower.java
+++ Recursion/PrintPower.java
@@ -1,11 +1,22 @@
 public class PrintPower {
-    public static int pow(int x, int n){
-        if (n == 0){
+    public static int pow(int x, int n) {
+        if (n == 0) {
             return 1;
         }
-        return x * pow(x , n-1);
+        return x * pow(x, n - 1);
     }
+
+    // this will have the run time complexity of O(log n)
+    public static int pow2(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int halfPower = pow2(x, n / 2);
+        int halfPowerSq = halfPower * halfPower;
+        return n % 2 == 0 ? halfPowerSq : (x * halfPowerSq);
+    }
+
     public static void main(String[] args) {
-        System.out.print(pow(3, 4));
+        System.out.print(pow2(2, 4));
     }
 }
